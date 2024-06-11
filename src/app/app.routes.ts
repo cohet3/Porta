4d5f4d9798d59c3c58cf4c7f395ef12a700f05c1
cohet3 <tauro.total90@gmail.com>
2024-06-11 21:49:10 +0200

    home completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ProyectsComponent } from './proyects/proyects.component';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'proyects', component: ProyectsComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: HomeComponent },
+    { path: 'home', component: HomeComponent }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ src/app/proyects/proyects.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-proyects',
+  standalone: true,
+  imports: [RouterModule, RouterLink],
+  templateUrl: './proyects.component.html',
+  styleUrl: './proyects.component.css'
+})
+export class ProyectsComponent {
+
+}
