ea78704274d3fee980594c306c72c9541be54141
cohet3 <tauro.total90@gmail.com>
2024-07-04 19:12:11 +0200

    commit de actulización

--- dist/porta/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-KAGU2RCZ.mjs";import"./chunk-NJ5BUI24.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KAGU2RCZ.mjs";import"./chunk-NJ5BUI24.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
