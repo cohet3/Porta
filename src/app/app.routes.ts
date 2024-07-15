2f3e4d00677a0f55536ccacf88ffa3493d7be32c
cohet3 <tauro.total90@gmail.com>
2024-07-15 12:52:43 +0200

    commit de actualización

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
+import { LogrosComponent } from './logros/logros.component';
 
 export const routes: Routes = [
     { path: 'proyects', component: ProyectsComponent },
     { path: 'resume', component: ResumeComponent },
     { path: 'contact', component: ContactComponent },
-    { path: '**', component: HomeComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'logros', component: LogrosComponent },
+    { path: '**', component: HomeComponent }
+
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
